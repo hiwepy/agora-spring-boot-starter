67aae0a60dbe165b1e9a24f86756fe13f844ece6
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:17:20 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/io/agora/spring/boot/req/RecordingFileConfig.java
+++ src/main/java/io/agora/spring/boot/req/RecordingFileConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * Recording file output configuration (format, storage path and so on).
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @JsonInclude( JsonInclude.Include.NON_NULL)
--- src/main/java/io/agora/spring/boot/req/TranscodingLayoutConfig.java
+++ src/main/java/io/agora/spring/boot/req/TranscodingLayoutConfig.java
@@ -7,7 +7,7 @@ import lombok.Data;
 /**
  * Layout configuration for transcoded composite streams.
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @JsonInclude( JsonInclude.Include.NON_NULL)
